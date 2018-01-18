b509ecd169d4e019381a88526aa307efe0c7a01b
Kim Paulissen <kim.paulissen@kuleuven.be>
2018-01-18 10:39:36 +0100

    add contenttype method and test gitignore

--- js/request-logic.js
+++ js/request-logic.js
@@ -5,7 +5,24 @@
 
 function getContentType(filename) {
   const extension = filename.match(/.*\.([^\.]*)$/)[1];
-  if (extension === 'html') {
 
+  switch (extension) {
+    case 'html':
+      return 'text/html';
+      break;
+    case 'css':
+      return 'text/css';
+      break;
+    case 'jpeg':
+      return 'image/jpeg';
+      break;
+    case 'jpg':
+      return 'image/jpeg';
+      break;
+    default:
+      return 'text/plain'
+      console.log('defaulting to text/plain');
+      break;
   }
+
 }
